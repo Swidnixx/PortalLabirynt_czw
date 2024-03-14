8af43158ff6677c6ca90a23cbb3c504f22c92120
Swidnixx <swidnixx@gmail.com>
2024-03-14 20:05:22 +0100

    Dokonczenie modeli pickupów

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float speed = 1;
+    public Vector3 axis = Vector3.forward;
+
+    private void Update()
+    {
+        transform.Rotate(axis * speed * Time.deltaTime, Space.Self);
+    }
+}
